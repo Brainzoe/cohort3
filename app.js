1605d5f8584ef9ac81f6a7112da7c4b85cad1157
Yusufjenom <jenomy32@gmail.com>
2023-12-14 14:10:31 -0800

    updated

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {connectDb} = require('./database/db');
 const BlogModel = require('./models/BlogModel');
+const methodOverride = require('method-override');
 const app = express();
 const port = 4000;
 
@@ -13,11 +14,12 @@ app.use(express.json());
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 //status code ok-200 create-201 //not-found-404 bad-request-400 401-unauthorized 402- payment needed 500- server side error
 //crud c-post r-get u-put d-delete
 
-//READ
+//READ START
 app.get('/', (req, res) => {
     try {
 
@@ -45,9 +47,48 @@ app.get('/make-post', (req, res) => {
     }
 });
 
+app.get('/view-blog', async (req, res) => {
+  try{
+    const blogs = await BlogModel.find();
+    //console.log(blogs);
+    res.status(200).render('viewBlog', {blogs});
+  }
+  catch(err){
+    console.log(err.message)
+  }
+});
 
 
-//CREATE
+app.get('/single-view/:id', async (req, res) => {
+    try{
+       const {id } = req.params;
+       //console.log(id);
+       const blog = await BlogModel.findById(id);
+       //console.log(blog);
+       res.status(200).render('singleView', {blog});
+       
+    }
+    catch(err){
+        console.log(err.message)
+    }
+})
+app.get('/update-blog/:id', async (req, res) => {
+    try{
+       const {id } = req.params;
+       console.log(id);
+       const blog = await BlogModel.findById(id);
+       console.log(blog);
+       res.status(200).render('update', {blog});
+       
+    }
+    catch(err){
+        console.log(err.message)
+    }
+});
+// READ END
+
+
+//CREATE/POST START
 app.post('/post-blog-api', async (req, res) => {
     try{
       const {blogname, email, subject, message} = req.body;
@@ -61,17 +102,35 @@ app.post('/post-blog-api', async (req, res) => {
       });
 
       const savedBlog = await newBlogDetail.save();
-      res.status(201).redirect('/');
-    //   res.status(201).json({
-        
-    //     savedBlog
-    //   })
+      res.status(201).redirect('/view-blog');
     }
     catch(err){
         console.log(err)
     }
-})
+});
 
+//CREATE/POST END
+
+
+//UPDATE START
+app.patch('/update-blog/:id', async (req, res) => {
+    try{
+        const {id} = req.params;
+      const {blogname, email, subject, message} = req.body;
+      const updatedBlog = await BlogModel.findByIdAndUpdate(id, {
+        name: blogname,
+        email,
+        subject,
+        message
+      });
+      res.status(200).redirect('/view-blog')
+    }
+    catch(err){
+        console.log(err)
+    }
+});
+
+//UPDATE END
 
 app.get("*", (req, res) => {
     try {
